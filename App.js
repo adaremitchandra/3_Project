54465b84f10aadeb093b64fde5a7a47e5b067a10
adaremitchandra <adaremitchandra@gmail.com>
2022-03-10 11:58:12 +0700

    Setup redux, dan install package

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import "react-native-gesture-handler";
+import { combineReducers, createStore } from "redux";
+import productReducer from "./store/reducers/product";
+import { Provider } from "react-redux";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const rootReducer = combineReducers({
+  products: productReducer,
 });
+
+const store = createStore(rootReducer);
+const App = () => {
+  return (
+    <Provider store={store}>
+      <View>
+        <Text>App</Text>
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React from "react";
+
+const ProductsOverviewScreen = () => {
+  return <Flatlist />;
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
