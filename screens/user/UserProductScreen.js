645e3e8ebb3dc99009981babdd913a33f66699c6
AdaremitChandra <adaremitchandra@gmail.com>
2022-03-17 14:10:22 +0700

    aktifkan Delete Product

--- screens/user/UserProductScreen.js
+++ screens/user/UserProductScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button, FlatList, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductItemComponent from "../../components/shop/ProductItemComponent";
 import Colors from "../../constants/Colors";
+import * as productAction from "../../store/actions/product";
+
 const UserProductScreen = () => {
   const userProduct = useSelector((state) => state.products.userProduct);
+  const dispatch = useDispatch();
   return (
     <FlatList
       data={userProduct}
@@ -17,7 +20,13 @@ const UserProductScreen = () => {
           onSelect={() => {}}
         >
           <Button color={Colors.primary} title="Edit" onPress={() => {}} />
-          <Button color={Colors.primary} title="Delete" onPress={() => {}} />
+          <Button
+            color={Colors.primary}
+            title="Delete"
+            onPress={() =>
+              dispatch(productAction.deleteProduct(itemData.item.id))
+            }
+          />
         </ProductItemComponent>
       )}
     />
--- store/reducers/product.js
+++ store/reducers/product.js
@@ -1,4 +1,5 @@
 import PRODUCTS from "../../data/dummy-data";
+import { DELETE_PRODUCT } from "../actions/product";
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -6,5 +7,17 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+  switch (action.type) {
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProduct: state.userProduct.filter(
+          (product) => product.id !== action.pid
+        ),
+        availableProducts: state.availableProducts.filter(
+          (product) => product.id !== action.pid
+        ),
+      };
+  }
   return state;
 };
